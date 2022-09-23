73a259d2d76e5126277481025c6cc33ba35e9d53
lordesuperfan <em6xt@virginia.edu>
2022-09-23 15:20:08 -0400

    ten!!!

--- Assets/Scripts/BrokenScripts/10/MoveObject10.cs
+++ Assets/Scripts/BrokenScripts/10/MoveObject10.cs
@@ -4,39 +4,43 @@ using UnityEngine;
 
 public class MoveObject10 : MonoBehaviour
 {
-    //float moveSpeed;
-    //[SerializeField] Vector3 moveDirection;
+    float moveSpeed = 0.01f;
+    [SerializeField] Vector3 moveDirection;
 
-    //totalMoveDistance;
-    //Vector3 startingLocation;
+    float totalMoveDistance;
+    Vector3 startingLocation;
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    totalMoveDistance = 10f;
-    //    startingLocation = gameObject.transform.position;
-    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalMoveDistance = 10f;
+        startingLocation = gameObject.transform.position;
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    float distanceTraveled = GetDistanceTraveled();
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceTraveled = GetDistanceTraveled();
 
-    //    if (distanceTraveled > totalMoveDistance)
-    //    {
-    //        FlipMoveDirection();
-    //    }
+        if (distanceTraveled > totalMoveDistance)
+        {
+            FlipMoveDirection();
+            this.enabled = false;
 
-    //    gameObject.transform.Translate(moveDirection * moveSpeed);
-    //}
+        }
 
-    //void FlipMoveDirection()
-    //{
-    //    moveDirection = -moveDirection;
-    //}
+        gameObject.transform.Translate(moveDirection * moveSpeed);
+    }
 
-    //float GetDistanceTraveled()
-    //{
-    //    return Vector3.Distance(gameObject.transform.position, startingLocation);
-    //}
+    void FlipMoveDirection()
+    {
+        moveDirection = -moveDirection;
+        startingLocation = gameObject.transform.position;
+
+    }
+
+    float GetDistanceTraveled()
+    {
+        return Vector3.Distance(gameObject.transform.position, startingLocation);
+    }
 }
